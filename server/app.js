72eead1cb945a8771633278d1864794e97a40481
miraculous11 <1183885461@qq.com>
2025-03-28 00:04:47 +0800

    配置了websocket以及实现发送消息

--- server/app.js
+++ server/app.js
@@ -1,66 +1,101 @@
-import express from 'express'
-import cors from 'cors'
-import db from '../db/index.js'
-import router from './router/user.js'
-import expressJWT from 'express-jwt'
-const app = express()
+import express from "express";
+import cors from "cors";
+import db from "../db/index.js";
+import router from "./router/user.js";
+import expressJWT from "express-jwt";
+import WebSocket from "ws";
+import http from "http";
+const app = express();
+
+//创建http服务器
+const HttpServer = http.createServer(app);
+//创建Websocket服务器
+const wss = new WebSocket.Server({ server: HttpServer });
+//存储连接的客户端
+const clients = new Set();
+
+//客户端连接
+wss.on("connection", (ws) => {
+  console.log("客户端连接成功");
+  clients.add(ws);
+
+  //监听websocket连接
+  ws.on("message", (message) => {
+    console.log("收到消息", message);
+    //广播消息给所有客户端
+    for (const client of clients) {
+      if (client.readyState === WebSocket.OPEN) {
+        // console.log("进入广播");
+        client.send(message);
+      }
+    }
+  });
+  //监听客户端断开连接
+  ws.on("close", () => {
+    console.log("客户端断开连接");
+    clients.delete(ws);
+  });
+});
+
+//启动服务器
+const PORT = 3000;
+HttpServer.listen(PORT, () => {
+  console.log(`服务器运行在 http://localhost:${PORT}`);
+});
 
 //将CORS注册为全局中间件
-app.use(cors())
+app.use(cors());
 
 //配置解析token的中间件
-app.use(expressJWT({secret:"yaoyao No1"}).unless({path:[/^\/api\//]}))
+app.use(expressJWT({ secret: "yaoyao No1" }).unless({ path: [/^\/api\//] }));
 
 //配置解析表单数据的中间件
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 //导入并注册路由
-app.use(router)
+app.use(router);
 
 //验证token的有效性的中间件
-app.use((req,res,next)=>{
-    const authHeader = req.headers['Authorization']
-    const token = authHeader && authHeader.split(' ')[1]
-    console.log('token:' + token);
+app.use((req, res, next) => {
+  const authHeader = req.headers["Authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  console.log("token:" + token);
 
-    if(!token){
-        //如果token不存在
-        return res.status(1).json({error:'No Token Provided'})
+  if (!token) {
+    //如果token不存在
+    return res.status(1).json({ error: "No Token Provided" });
+  }
+
+  //验证token有效性
+  jwt.verify(token, (err, user) => {
+    if (err) {
+      return res.status(1).json({ error: "Invalid Token" });
     }
-
-    //验证token有效性
-    jwt.verify(token,(err,user)=>{
-        if(err){
-            return res.status(1).json({error:'Invalid Token'})
-        }
-        //将用户信息附加到请求对象
-        req.user = user
-        next()
-    })
-})
+    //将用户信息附加到请求对象
+    req.user = user;
+    next();
+  });
+});
 
 //配置错误中间件
-app.use((err,req,res,next)=>{
-    if(err.name === 'UnauthorizedError'){
-        return res.send({
-            status:1,
-            message:'身份认证失败'
-        })
-    }
-})
-
+app.use((err, req, res, next) => {
+  if (err.name === "UnauthorizedError") {
+    return res.send({
+      status: 1,
+      message: "身份认证失败",
+    });
+  }
+});
 
 // 测试连接
-db.query('select 1',(err,result)=>{
-    if(err){
-        return console.log(err);
-    }
-    console.log(result);
-})
-
-
+db.query("select 1", (err, result) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(result);
+});
 
 //监听
-app.listen(8080,()=>{
-    console.log('server running at http://127.0.0.1:8080');
-})
+app.listen(8080, () => {
+  console.log("server running at http://127.0.0.1:8080");
+});
--- src/components/index.vue
+++ src/components/index.vue
@@ -7,101 +7,151 @@ const textarea = ref("");
 const isActive = ref(0);
 
 //定义一个数据
-const UserData = ref([{
-  username: "miraculous",
-  avator:
-    "https://img2.baidu.com/it/u=3739988182,1148289226&fm=253&fmt=auto&app=120&f=JPEG?w=500&h=689",
-  message: "你好1",
-},
-{
-  username: "miraculous1",
-  avator:
-    "https://img2.baidu.com/it/u=3739988182,1148289226&fm=253&fmt=auto&app=120&f=JPEG?w=500&h=689",
-  message: "你好2",
-},
-{
-  username: "miraculous2",
-  avator:
-    "https://img2.baidu.com/it/u=3739988182,1148289226&fm=253&fmt=auto&app=120&f=JPEG?w=500&h=689",
-  message: "你好3",
-},
-{
-  username: "miraculous3",
-  avator:
-    "https://img2.baidu.com/it/u=3739988182,1148289226&fm=253&fmt=auto&app=120&f=JPEG?w=500&h=689",
-  message: "你好",
-},
-{
-  username: "miraculous4",
-  avator:
-    "https://img2.baidu.com/it/u=3739988182,1148289226&fm=253&fmt=auto&app=120&f=JPEG?w=500&h=689",
-  message: "你好",
-},
-{
-  username: "miraculous5",
-  avator:
-    "https://img2.baidu.com/it/u=3739988182,1148289226&fm=253&fmt=auto&app=120&f=JPEG?w=500&h=689",
-  message: "你好",
-},
-{
-  username: "miraculous6",
-  avator:
-    "https://img2.baidu.com/it/u=3739988182,1148289226&fm=253&fmt=auto&app=120&f=JPEG?w=500&h=689",
-  message: "你好",
-},
-{
-  username: "miraculous7",
-  avator:
-    "https://img2.baidu.com/it/u=3739988182,1148289226&fm=253&fmt=auto&app=120&f=JPEG?w=500&h=689",
-  message: "你好",
-},
-{
-  username: "miraculous8",
-  avator:
-    "https://img2.baidu.com/it/u=3739988182,1148289226&fm=253&fmt=auto&app=120&f=JPEG?w=500&h=689",
-  message: "你好",
-},
-{
-  username: "miraculous9",
-  avator:
-    "https://img2.baidu.com/it/u=3739988182,1148289226&fm=253&fmt=auto&app=120&f=JPEG?w=500&h=689",
-  message: "你好",
-},
-{
-  username: "miraculous10",
-  avator:
-    "https://img2.baidu.com/it/u=3739988182,1148289226&fm=253&fmt=auto&app=120&f=JPEG?w=500&h=689",
-  message: "你好",
-},
-{
-  username: "miraculous11",
-  avator:
-    "https://img2.baidu.com/it/u=3739988182,1148289226&fm=253&fmt=auto&app=120&f=JPEG?w=500&h=689",
-  message: "你好",
-},
-{
-  username: "miraculous12",
-  avator:
-    "https://img2.baidu.com/it/u=3739988182,1148289226&fm=253&fmt=auto&app=120&f=JPEG?w=500&h=689",
-  message: "你好",
-},
-
+const UserData = ref([
+  {
+    username: "miraculous",
+    avator:
+      "https://img2.baidu.com/it/u=3739988182,1148289226&fm=253&fmt=auto&app=120&f=JPEG?w=500&h=689",
+    message: "你好1",
+  },
+  {
+    username: "miraculous1",
+    avator:
+      "https://img2.baidu.com/it/u=3739988182,1148289226&fm=253&fmt=auto&app=120&f=JPEG?w=500&h=689",
+    message: "你好2",
+  },
+  {
+    username: "miraculous2",
+    avator:
+      "https://img2.baidu.com/it/u=3739988182,1148289226&fm=253&fmt=auto&app=120&f=JPEG?w=500&h=689",
+    message: "你好3",
+  },
+  {
+    username: "miraculous3",
+    avator:
+      "https://img2.baidu.com/it/u=3739988182,1148289226&fm=253&fmt=auto&app=120&f=JPEG?w=500&h=689",
+    message: "你好",
+  },
+  {
+    username: "miraculous4",
+    avator:
+      "https://img2.baidu.com/it/u=3739988182,1148289226&fm=253&fmt=auto&app=120&f=JPEG?w=500&h=689",
+    message: "你好",
+  },
+  {
+    username: "miraculous5",
+    avator:
+      "https://img2.baidu.com/it/u=3739988182,1148289226&fm=253&fmt=auto&app=120&f=JPEG?w=500&h=689",
+    message: "你好",
+  },
+  {
+    username: "miraculous6",
+    avator:
+      "https://img2.baidu.com/it/u=3739988182,1148289226&fm=253&fmt=auto&app=120&f=JPEG?w=500&h=689",
+    message: "你好",
+  },
+  {
+    username: "miraculous7",
+    avator:
+      "https://img2.baidu.com/it/u=3739988182,1148289226&fm=253&fmt=auto&app=120&f=JPEG?w=500&h=689",
+    message: "你好",
+  },
+  {
+    username: "miraculous8",
+    avator:
+      "https://img2.baidu.com/it/u=3739988182,1148289226&fm=253&fmt=auto&app=120&f=JPEG?w=500&h=689",
+    message: "你好",
+  },
+  {
+    username: "miraculous9",
+    avator:
+      "https://img2.baidu.com/it/u=3739988182,1148289226&fm=253&fmt=auto&app=120&f=JPEG?w=500&h=689",
+    message: "你好",
+  },
+  {
+    username: "miraculous10",
+    avator:
+      "https://img2.baidu.com/it/u=3739988182,1148289226&fm=253&fmt=auto&app=120&f=JPEG?w=500&h=689",
+    message: "你好",
+  },
+  {
+    username: "miraculous11",
+    avator:
+      "https://img2.baidu.com/it/u=3739988182,1148289226&fm=253&fmt=auto&app=120&f=JPEG?w=500&h=689",
+    message: "你好",
+  },
+  {
+    username: "miraculous12",
+    avator:
+      "https://img2.baidu.com/it/u=3739988182,1148289226&fm=253&fmt=auto&app=120&f=JPEG?w=500&h=689",
+    message: "你好",
+  },
 ]);
 
+//定义消息数组
+const messages_other = ref([
+  {
+    id: 1,
+    message: "让我们红尘作伴",
+  },
+  {
+    id: 2,
+    message: "活得潇潇洒洒",
+  },
+  {
+    id: 3,
+    message: "策马奔腾",
+  },
+]);
+const messages_my = ref([]);
+
+//创建websocket连接
+const socket = new WebSocket("http://localhost:3000");
+
+//监听websocket链接
+socket.onopen = () => {
+  console.log("连接到服务器");
+};
+const messagesDiv = document.getElementById("main");
+const messageInput = document.getElementsByClassName("InputArea");
+
+//监听服务器消息
+socket.onmessage = (event) => {
+  const message = event.data;
+  console.log("消息为：" + message);
+  messages_my.value.push({
+    id: messages_my.value.length + 1,
+    message: message,
+  });
+};
+//监听websocket错误
+socket.onerror = (err) => {
+  console.log("Websocket错误:", err);
+};
+
+//点击发送的回调函数
+const onSubmit = () => {
+  // console.log("发送内容为：" + textarea.value);
+  socket.send(textarea.value);
+  textarea.value = "";
+};
+
+//监听输入框输入回车时发送消息
+
 //切换用户回调函数
-const onChangeUserItem = (index)=>{
-  isActive.value = index
-}
+const onChangeUserItem = (index) => {
+  isActive.value = index;
+};
 </script>
 
 <template>
   <div class="common-layout">
     <el-container>
       <el-aside class="aside" width="200px">
-        <el-scrollbar :always=false>
+        <el-scrollbar :always="false">
           <!-- 侧边栏用户栏 -->
           <div
-            v-for="(item,index) in UserData"
+            v-for="(item, index) in UserData"
             :key="item.id"
             class="userItem"
             :class="isActive === index ? 'ItemActive' : 'ItemUsual'"
@@ -113,7 +163,7 @@ const onChangeUserItem = (index)=>{
               size="8px"
               :src="item.avator"
             />
-            <span>{{ item.username}}</span>
+            <span>{{ item.username }}</span>
           </div>
         </el-scrollbar>
       </el-aside>
@@ -122,7 +172,11 @@ const onChangeUserItem = (index)=>{
           {{ UserData[isActive].username }}
         </el-header>
         <el-main class="main">
-          <div class="message">
+          <div
+            class="message_other"
+            v-for="(item, index) in messages_other"
+            :key="item.id"
+          >
             <el-avatar
               style="margin-left: 10px; margin-right: 10px"
               shape="square"
@@ -131,8 +185,24 @@ const onChangeUserItem = (index)=>{
             />
             <span
               style="padding: 5px; background-color: white; line-height: 30px"
-              >{{ UserData[isActive].message }}</span
+              >{{ item.message }}</span
             >
+          </div>
+          <div
+            class="message_my"
+            v-for="(item, index) in messages_my"
+            :key="item.id"
+          >
+            <span
+              style="padding: 5px; background-color: greenyellow; line-height: 30px"
+              >{{ item.message }}</span
+            >
+            <el-avatar
+              style="margin-right: 10px; margin-left: 10px"
+              shape="square"
+              size="10px"
+              :src="UserData[isActive].avator"
+            />
           </div>
         </el-main>
         <el-footer class="footer">
@@ -141,8 +211,10 @@ const onChangeUserItem = (index)=>{
             :rows="7"
             type="textarea"
             placeholder="请输入内容"
+            class="InputArea"
+            @keydown.enter.exact.prevent="onSubmit"
           />
-          <el-button type="primary">发送</el-button>
+          <el-button @click="onSubmit" type="primary">发送</el-button>
         </el-footer>
       </el-container>
     </el-container>
@@ -175,6 +247,8 @@ const onChangeUserItem = (index)=>{
   flex: 0.75;
   border-bottom: 1px solid rgb(218, 213, 213);
   border-left: 1px solid rgb(218, 213, 213);
+  display: flex;
+  flex-direction: column;
 }
 .footer {
   display: flex;
@@ -193,13 +267,23 @@ const onChangeUserItem = (index)=>{
 .footer .el-button {
   margin-left: 10px; /* 添加一些左边距，使按钮与文本内容分开 */
 }
-.message {
+.message_other {
   background-color: rgb(246, 248, 248);
   width: 200px;
   display: flex;
   justify-content: start;
   align-items: center;
   height: auto;
+  margin-bottom: 15px;
+}
+.message_my {
+  background-color: rgb(246, 248, 248);
+  width: 200px;
+  display: flex;
+  justify-content: end;
+  align-items: center;
+  margin-bottom: 15px;
+  margin-left: 250px;
 }
 .userItem {
   height: 70px;
